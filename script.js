fab555a34fb74070ca15f3848858101034d94bb7
Margul7803 <83760967+Margul7803@users.noreply.github.com>
2021-10-18 20:32:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function initElement()
         se.play();
     }
 }
+
+var button = document.getElementsByTagName('button')[0];
+var image = document.getElementsByTagName('dancin')[0];
+
+window.afficher = function(){
+image.style.display = (image.style.display == "block") ? "none" : "block";
+}
+
+
+
+button.onclick = afficher;
